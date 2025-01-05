7beaeb47e5c321bf7b898a064b50cc0aa80828e0
! Dr <119114346+7rbaldol@users.noreply.github.com>
2025-01-05 13:04:47 +0300

    Update script.js

--- script.js
+++ script.js
@@ -18,3 +18,20 @@ function toggleMenu() {
   const menu = document.getElementById("menu");
   menu.classList.toggle("open"); // إضافة أو إزالة الكلاس "open"
 }
+window.onload = () => {
+let Lodding = document.getElementById('Lodding');
+let loadingScreen = document.querySelector('.loading-screen');
+let progressValue = 0;
+
+ 
+let interval = setInterval(() => {
+if(progressValue < 100) {
+progressValue += 1;
+Lodding.value = progressValue;
+} else {
+
+clearInterval(interval);
+loadingScreen.classList.add('hidden');
+}
+}, 30);
+};
