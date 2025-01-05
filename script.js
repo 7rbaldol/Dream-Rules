2bf755032589f810eb404de61d1060b7bd21cbe9
! Dr <119114346+7rbaldol@users.noreply.github.com>
2025-01-05 13:06:52 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,3 @@
-// توليد الشهب
-function createStar() {
-  const star = document.createElement("div");
-  star.className = "shooting-star";
-  star.style.top = `${Math.random() * -20}vh`;
-  star.style.left = `${Math.random() * 100}vw`;
-  document.getElementById("stars").appendChild(star);
-  
-  // إزالة النجم بعد انتهاء الحركة
-  setTimeout(() => star.remove(), 4000); 
-}
-
-// إنشاء الشهب بشكل دوري
-setInterval(createStar, 700);
-
-// تفعيل قائمة الهامبورجر
-function toggleMenu() {
-  const menu = document.getElementById("menu");
-  menu.classList.toggle("open"); // إضافة أو إزالة الكلاس "open"
-}
 window.onload = () => {
 let Lodding = document.getElementById('Lodding');
 let loadingScreen = document.querySelector('.loading-screen');
