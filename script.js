e4c346bd8346b3475124ac11bdbc94535b7efc5c
! Dr <119114346+7rbaldol@users.noreply.github.com>
2025-01-05 13:29:00 +0300

    Update script.js

--- script.js
+++ script.js
@@ -15,3 +15,16 @@ loadingScreen.classList.add('hidden');
 }
 }, 30);
 };
+// منع F12 و Ctrl+Shift+I
+document.addEventListener("keydown", function (e) {
+  if ((e.key === "F12") || (e.ctrlKey && e.shiftKey && e.key === "I")) {
+    e.preventDefault(); // منع تنفيذ الاختصار
+    alert("أدوات المطور تم تعطيلها.");
+  }
+});
+
+// منع زر الفأرة الأيمن (Right Click)
+document.addEventListener("contextmenu", function (e) {
+  e.preventDefault(); // منع القائمة السياقية
+  alert("القائمة السياقية تم تعطيلها.");
+});
