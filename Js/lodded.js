67d7d1bad3137fa7180bdf0c601e8e4ac374ba2e
! Dr <119114346+7rbaldol@users.noreply.github.com>
2025-01-07 01:21:25 +0300

    Update lodded.js

--- Js/lodded.js
+++ Js/lodded.js
@@ -1,6 +1,6 @@
-/*fetch(`include/footer.html`).then(res => res.text()).then(data => {
+fetch(`/pages/include/footer.html`).then(res => res.text()).then(data => {
     document.getElementById('footer').innerHTML = data
 })
-fetch(`include/header.html`).then(res => res.text()).then(data => {
+fetch(`/pages/include/header.html`).then(res => res.text()).then(data => {
     document.getElementById('header').innerHTML = data
-})*/
+})
