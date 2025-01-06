de79091468538447bcd7602354d6ca3b833d352c
! Dr <119114346+7rbaldol@users.noreply.github.com>
2025-01-07 01:14:37 +0300

    Add files via upload

---
+++ Js/lodded.js
@@ -0,0 +1,6 @@
+fetch(`include/footer.html`).then(res => res.text()).then(data => {
+    document.getElementById('footer').innerHTML = data
+})
+fetch(`include/header.html`).then(res => res.text()).then(data => {
+    document.getElementById('header').innerHTML = data
+})
