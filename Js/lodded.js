45c74cc9ab0a2f5d6320a9bc5267925161cc54b5
! Dr <119114346+7rbaldol@users.noreply.github.com>
2025-01-07 10:51:07 +0300

    Update lodded.js

--- Js/lodded.js
+++ Js/lodded.js
@@ -1,11 +1,20 @@
-fetch('https://bot.dream-bot.online/pages/include/footer.html')
+/*fetch('https://bot.dream-bot.online/pages/include/footer.html')
     .then(res => res.text())
     .then(data => {
         document.getElementById('footer').innerHTML = data;
-    });
-
-fetch('https://bot.dream-bot.online/pages/include/header.html')
+    });*/
+ document.getElementById('footer').innerHTML = `<footer class="footer">
+    <div class="footer-content">
+      <p>© 2024 Dream Bot | جميع الحقوق محفوظة.</p>
+      <div class="social-icons">
+        <a href="https://discord.gg/bY5JWM4N5p" class="icon"><i class="fab fa-discord"></i></a>
+      </div>
+    </div>
+  </footer>
+  `
+document.getElementById('header').innerHTML = 
+/*fetch('https://bot.dream-bot.online/pages/include/header.html')
     .then(res => res.text())
     .then(data => {
         document.getElementById('header').innerHTML = data;
-    });
+    });*/
